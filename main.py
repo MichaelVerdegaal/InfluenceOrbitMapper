c56f945d1f641882e1fc67ae6dd2a6f6d64b6214
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2021-09-20 22:56:24 +0200

    Trying out orbit calculation methods

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import matplotlib
 
 from asteroids import load_roids, get_roid
 from plotting import plot_asteroid_paths
+from orbits import position_at_adalia_day, position_at_adalia_day_manual
 
 # Initialization
 print('Booting up...')
@@ -9,8 +10,25 @@ matplotlib.use("TkAgg")  # Use the Tkinter backend to display animations
 roids = load_roids("asteroids_20210917.json")
 
 # Get rock of choice
-adalia_prime = get_roid(roids, 1)
-saturnus = get_roid(roids, 249999)
+rock_1 = get_roid(roids, 1)
 
 # print('Plotting rocks')
-plot_asteroid_paths(saturnus, adalia_prime)
+# plot_asteroid_paths(saturnus, adalia_prime)
+
+x, y, z = rock_1['p.x'], rock_1['p.y'], rock_1['p.z']
+T = rock_1['orbital.T']
+print(f"Expected XYZ coords of {rock_1['customName']}:[{rock_1['p.x']}, {rock_1['p.y']}, {rock_1['p.z']}] "
+      f"with orbital period {T}\n")
+
+# Attempt at reverse engineering the orbit calculation
+listy = []
+for day in range(T * 24):
+    pos = position_at_adalia_day_manual(rock_1, day)
+    x1_round, x2_round = round(pos[0], 3), round(x, 3)
+    x_delta = x1_round == x2_round
+    if x_delta:
+        listy.append({'T': day, 'p': pos, 'true': [x, y, z]})
+
+for i in listy:
+    print(i)
+print(len(listy))
--- orbits.py
+++ orbits.py
@@ -1,3 +1,5 @@
+import math
+
 import arrow
 import numpy as np
 from PyAstronomy import pyasl
@@ -42,6 +44,52 @@ def position_at_adalia_day(rock, adalia_day):
         return [0, 0, 0]
 
 
+def position_at_adalia_day_manual(rock, adalia_day):
+    """
+    Direct copy of https://github.com/Influenceth/influence-utils/blob/00f6838b616d5c7113720b0f883c2a2d55a41267/index.js#L288
+    Translated from JS to Python
+    :param rock:
+    :param adalia_day:
+    :return:
+    """
+    a = rock['orbital.a']
+    e = rock['orbital.e']
+    i = rock['orbital.i']
+    o = rock['orbital.o']
+    w = rock['orbital.w']
+    m = rock['orbital.m']
+
+    # Calculate the longitude of perihelion
+    p = w + o
+
+    # Calculate mean motion based on assumption that mass of asteroid <<< Sun
+    k = 0.01720209895  # Gaussian constant (units are days and AU)
+    n = k / math.sqrt(math.pow(a, 3))  # Mean motion
+
+    # Calcualate the mean anomoly at elapsed time
+    M = m + (n * adalia_day)
+
+    # Estimate the eccentric and true anomolies using an iterative approximation let E1let E = M let lastDiff = 1
+    E1 = None
+    E = M
+    last_diff = 1
+
+    while last_diff > 0.0000001:
+        E1 = M + (e * math.sin(E))
+        last_diff = np.abs(E1 - E)
+        E = E1
+
+    # Calculate in heliocentric polar and then convert to cartesian
+    v = 2 * math.atan(math.sqrt((1 + e) / (1 - e)) * math.tan(E / 2))
+    r = a * (1 - math.pow(e, 2)) / (1 + e * math.cos(v))  # Current radius in AU
+
+    # Cartesian coordinates
+    x = r * (math.cos(o) * math.cos(v + p - o) - (math.sin(o) * math.sin(v + p - o) * math.cos(i)))
+    y = r * (math.sin(o) * math.cos(v + p - o) + math.cos(o) * math.sin(v + p - o) * math.cos(i))
+    z = r * math.sin(v + p - o) * math.sin(i)
+    return np.array([x, y, z])
+
+
 def full_position(rock):
     orbit = setup_ellipse(rock)
     t = np.linspace(0, rock['orbital.T'], rock['orbital.T'])
@@ -55,8 +103,8 @@ def get_current_adalia_day():
     """
     start_time = arrow.get(START_TIMESTAMP)
     current_time = arrow.utcnow()
-    # Time diff in seconds then hours to get adalia days. 1 irl hour = 1 adalia day. Converted from seconds
-    # as it's more precise.
+    # Time diff in seconds then hours to get adalia days. 1 irl hour = 1 adalia day. Converted from seconds instead of
+    # days as it's more precise.
     adalia_days = (current_time - start_time).total_seconds() / 60 / 60
     return adalia_days
 
