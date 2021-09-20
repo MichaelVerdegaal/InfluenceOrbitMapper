34571e6c6ab0d4cb133f27798c06678d484f551c
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2021-09-20 23:22:15 +0200

    Stop I conversion to degrees
    Radians seems to work better for the manual orbit calculation, as it gives odd results with degrees

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ roids = load_roids("asteroids_20210917.json")
 
 # Get rock of choice
 rock_1 = get_roid(roids, 1)
+rock_2 = get_roid(roids, 249999)
 
 # print('Plotting rocks')
-# plot_asteroid_paths(saturnus, adalia_prime)
+# plot_asteroid_paths(rock_1, rock_2)
 
 x, y, z = rock_1['p.x'], rock_1['p.y'], rock_1['p.z']
 T = rock_1['orbital.T']
@@ -24,9 +25,13 @@ print(f"Expected XYZ coords of {rock_1['customName']}:[{rock_1['p.x']}, {rock_1[
 listy = []
 for day in range(T * 24):
     pos = position_at_adalia_day_manual(rock_1, day)
-    x1_round, x2_round = round(pos[0], 3), round(x, 3)
-    x_delta = x1_round == x2_round
-    if x_delta:
+    x1_round, x2_round = round(pos[0], 2), round(x, 2)
+    x_match = x1_round == x2_round
+    y1_round, y2_round = round(pos[1], 2), round(y, 2)
+    y_match = y1_round == y2_round
+    z1_round, z2_round = round(pos[2], 2), round(z, 2)
+    z_match = z1_round == z2_round
+    if x_match and y_match and z_match:
         listy.append({'T': day, 'p': pos, 'true': [x, y, z]})
 
 for i in listy:
