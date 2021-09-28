917832c3c3cb00e4b28d73669236b5f16361985d
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2021-09-28 20:18:49 +0200

    Plot all orbits

--- viewer/frontend/static/js/asteroid.js
+++ viewer/frontend/static/js/asteroid.js
@@ -1,6 +1,6 @@
 let view_layout = {
-    plot_bgcolor: "rgb(13,13,13)",
-    paper_bgcolor: "rgb(13,13,13)",
+    plot_bgcolor: "rgb(5,5,5)",
+    paper_bgcolor: "rgb(5,5,5)",
     'height': '100vh',
     title: '3D orbits',
     autosize: true,
@@ -38,6 +38,35 @@ let view_layout = {
     }
 };
 
+
+function orbitTrace(fullPosition) {
+    /**
+     * Creates a plotly trace
+     * @param fullPosition - List of xyz coordinates
+     */
+    return {
+        x: fullPosition.map(function (value) {
+            return value[0];
+        }),
+        y: fullPosition.map(function (value) {
+            return value[1];
+        }),
+        z: fullPosition.map(function (value) {
+            return value[2];
+        }),
+        mode: 'markers',
+        marker: {
+            size: 0.3,
+            line: {
+                color: 'white',
+                width: 5
+            },
+        },
+        type: 'scatter3d'
+    }
+}
+
+
 function asteroidGet(urlBase) {
     /**
      * Retrieves an asteroid
@@ -64,31 +93,17 @@ function asteroidGet(urlBase) {
     postRequest(urlBase, {start_asteroids: startAsteroidIDList, target_asteroids: endAsteroidIDList})
         .then(isOk)
         .then(response => {
-            console.log(response);
-            let sorbet = response['starting_orbits'][0]['orbit']
+            let startingOrbits = response['starting_orbits'];
+            let targetOrbits = response['target_orbits'];
+            let data = []
 
-            let data = [
-                {
-                    x: sorbet.map(function (value, index) {
-                        return value[0];
-                    }),
-                    y: sorbet.map(function (value, index) {
-                        return value[1];
-                    }),
-                    z: sorbet.map(function (value, index) {
-                        return value[2];
-                    }),
-                    mode: 'markers',
-                    marker: {
-                        size: 0.3,
-                        line: {
-                            color: 'white',
-                            width: 5
-                        },
-                    },
-                    type: 'scatter3d'
-                }
-            ]
+            for (let orbit of startingOrbits) {
+                data.push(orbitTrace(orbit['orbit']));
+            }
+            for (let orbit of targetOrbits) {
+                data.push(orbitTrace(orbit['orbit']));
+            }
+
 
             Plotly.newPlot('view', data, view_layout, {'responsive': true});
             window.dispatchEvent(new Event('resize')); // Plotly graph doesn't fill screen until window resize
