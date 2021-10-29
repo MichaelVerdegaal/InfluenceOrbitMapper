12984fcbdd1bba663a9c1519465df686c93fd3cc
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2021-10-29 23:35:21 +0200

    Re-enable caching
    Sadly flask-minify had to be lost as it can't be patched. An alternative was quart-minify but that package is too outdated and won't run anymore

--- viewer/__init__.py
+++ viewer/__init__.py
@@ -1,23 +1,20 @@
 """Flask app creation helper."""
+import quart.flask_patch  # Must be top level to patch Flask extensiosn
 import os
 
 import sass
+from flask_caching import Cache
 from quart import Quart
-# from flask_caching import Cache
-# from flask_minify import minify
+
 from viewer.config import STATIC_FOLDER
 
 # Caching
-# TODO: Handle these middleware
-# cache = Cache(config={'CACHE_TYPE': 'simple'})
+cache = Cache(config={'CACHE_TYPE': 'simple'})
 # Compile SASS to CSS
 sass.compile(dirname=(os.path.join(STATIC_FOLDER, 'sass'), os.path.join(STATIC_FOLDER, 'css')),
              output_style='compressed')
 
-
 def create_app():
     app = Quart(__name__, template_folder='frontend/templates/', static_folder='frontend/static/')
-    # TODO: Handle these middleware
-    # minify(app=app, html=True, js=True, cssless=False)
-    # cache.init_app(app)
+    cache.init_app(app)
     return app
